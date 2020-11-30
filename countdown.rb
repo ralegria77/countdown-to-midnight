b8d34d63caee4055d0b0728340e1ef4e5b174a48
rebec <ralegria77@gmail.com>
2020-11-29 23:03:45 -0800

    commit tree eb469b625463afe46c64b6154cdffb7e0888e524

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,12 @@
-#write your code here
-
-def countdown(seconds)
-    counter = 10
-    while counter >0
-        puts "#{counter} SECOND(S)!"
-        counter -=1
+def countdown(i)
+    i = 10
+    while i >0
+        puts "#{i} SECOND(S)!"
+        i -= 1
     end
-    "HAPPY NEW YEAR!"
+    return "HAPPY NEW YEAR!"
 end
+
 
 def countdown_with_sleep(seconds)
     counter = 5
